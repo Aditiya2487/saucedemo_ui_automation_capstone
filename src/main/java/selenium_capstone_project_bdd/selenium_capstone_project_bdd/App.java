92feab14dae1a99d004fe7271a5405d8065ea4c7
Aditiya2487 <aditiya.209202191@muj.manipal.edu>
2025-09-08 09:28:11 +0530

    first commit

---
+++ src/main/java/selenium_capstone_project_bdd/selenium_capstone_project_bdd/App.java
@@ -0,0 +1,13 @@
+package selenium_capstone_project_bdd.selenium_capstone_project_bdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
