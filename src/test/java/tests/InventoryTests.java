7c40c2cde4655eac47cd0c5b0cbc37e690041f1e
Aditiya2487 <aditiya.209202191@muj.manipal.edu>
2025-09-08 20:08:35 +0530

    Initial commit with Selenium Capstone project

---
+++ src/test/java/tests/InventoryTests.java
@@ -0,0 +1,103 @@
+package tests;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.InventoryPage;
+import pages.LoginPage;
+import utils.DriverFactory;
+
+public class InventoryTests {
+
+    @BeforeMethod
+    public void setUp() {
+        LoginPage login = new LoginPage();
+        login.open();
+        login.waitFor(2);
+        login.login("standard_user", "secret_sauce");
+        login.waitFor(3); 
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+
+    @Test
+    public void verifyProductsDisplayed() {
+        new InventoryPage().waitFor(2);
+        Assert.assertTrue(DriverFactory.getDriver().getPageSource().contains("Sauce Labs Backpack"),
+                "Products are not displayed after login");
+    }
+
+    @Test
+    public void addSingleProductToCart() {
+        InventoryPage inventory = new InventoryPage();
+        inventory.addBackpackToCart();
+        inventory.waitFor(3); 
+        Assert.assertTrue(DriverFactory.getDriver().getPageSource().contains("Remove"),
+                "Product not added to cart");
+    }
+
+    @Test
+    public void addMultipleProductsToCart() {
+        InventoryPage inventory = new InventoryPage();
+        inventory.addBackpackToCart();
+        inventory.waitFor(2);
+        DriverFactory.getDriver().findElement(
+                org.openqa.selenium.By.xpath("//div[text()='Sauce Labs Bike Light']/ancestor::div[@class='inventory_item']//button")
+        ).click();
+        inventory.waitFor(3); 
+        Assert.assertTrue(DriverFactory.getDriver().getPageSource().contains("Remove"),
+                "Multiple products not added correctly");
+    }
+
+    @Test
+    public void removeProductFromCart() {
+        InventoryPage inventory = new InventoryPage();
+        inventory.addBackpackToCart();
+        inventory.waitFor(2);
+        DriverFactory.getDriver().findElement(
+                org.openqa.selenium.By.xpath("//div[text()='Sauce Labs Backpack']/ancestor::div[@class='inventory_item']//button")
+        ).click();
+        inventory.waitFor(3);
+        Assert.assertFalse(DriverFactory.getDriver().getPageSource().contains("Remove"),
+                "Product was not removed successfully");
+    }
+
+    @Test
+    public void sortProductsByNameAZ() {
+        org.openqa.selenium.support.ui.Select sortDropdown =
+                new org.openqa.selenium.support.ui.Select(DriverFactory.getDriver().findElement(org.openqa.selenium.By.className("product_sort_container")));
+        sortDropdown.selectByVisibleText("Name (A to Z)");
+        new InventoryPage().waitFor(3); 
+        Assert.assertTrue(true, "Products sorted by Name A-Z (manual visual validation)");
+    }
+
+    @Test
+    public void sortProductsByNameZA() {
+        org.openqa.selenium.support.ui.Select sortDropdown =
+                new org.openqa.selenium.support.ui.Select(DriverFactory.getDriver().findElement(org.openqa.selenium.By.className("product_sort_container")));
+        sortDropdown.selectByVisibleText("Name (Z to A)");
+        new InventoryPage().waitFor(3);
+        Assert.assertTrue(true, "Products sorted by Name Z-A (manual visual validation)");
+    }
+
+    @Test
+    public void sortProductsByPriceLowToHigh() {
+        org.openqa.selenium.support.ui.Select sortDropdown =
+                new org.openqa.selenium.support.ui.Select(DriverFactory.getDriver().findElement(org.openqa.selenium.By.className("product_sort_container")));
+        sortDropdown.selectByVisibleText("Price (low to high)");
+        new InventoryPage().waitFor(3);
+        Assert.assertTrue(true, "Products sorted Low to High (manual visual validation)");
+    }
+
+    @Test
+    public void sortProductsByPriceHighToLow() {
+        org.openqa.selenium.support.ui.Select sortDropdown =
+                new org.openqa.selenium.support.ui.Select(DriverFactory.getDriver().findElement(org.openqa.selenium.By.className("product_sort_container")));
+        sortDropdown.selectByVisibleText("Price (high to low)");
+        new InventoryPage().waitFor(3);
+        Assert.assertTrue(true, "Products sorted High to Low (manual visual validation)");
+    }
+}
